dab29442c24178fe52070aec2f93a189b5b261bd
Byron <byronwang@pacewear.com>
2018-05-14 09:39:11 +0800

    add insert data button

--- app/src/main/java/com/byron/databasetest/MainActivity.java
+++ app/src/main/java/com/byron/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.byron.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        databaseHelper = new MyDatabaseHelper(this, "BookStore.db", null, 5);
     }
 
     public void onButtonClick(View view) {
-        databaseHelper.getWritableDatabase();
+        switch (view.getId()) {
+            case R.id.create_db_button:
+                databaseHelper.getWritableDatabase();
+                break;
+            case R.id.add_daba_button:
+                SQLiteDatabase db = databaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+                values.clear();
+                values.put("name", "The lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+                break;
+        }
     }
 }
